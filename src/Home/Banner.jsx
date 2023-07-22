73dd76d84eef58888e9f546ce3cf0de629735aa0
Your Name <you@example.com>
2023-07-22 14:27:55 +0600

    navber and footer added

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import student from '../assets/student.png'
 import { TypeAnimation } from 'react-type-animation';
+import Navber from './Navber';
 
 const Banner = () => {
     return (
         <div>
-            <div className='w-full banner-bg'>
+            <div className='w-full banner-bg bg-fixed'>
+                <div className='mx-20 py-5'>
+                    <Navber></Navber>
+                </div>
                 <div className=' md:flex justify-between px-20'>
                     <div className='my-auto w-1/2'>
                         <h3 className='text-4xl text-white font-light '>Welcome to Oneirocollage</h3>
                         <TypeAnimation
                             sequence={[
-                                
+
                                 `You can here Search`,
                                 1000, // wait 1s before replacing "Mice" with "Hamsters"
                                 'You can here Find',
                                 1000,
                                 'You can here Apply',
                                 1000,
-                                
+
                             ]}
                             wrapper="span"
                             speed={50}
-                            style={{ fontSize: '50px', display: 'inline-block' , color: 'white' }}
+                            style={{ fontSize: '50px', display: 'inline-block', color: 'white' }}
                             repeat={Infinity}
                         />
                         <p className=' text-white text-[14px]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi eos labore quo cumque consequatur dolores, est beatae vitae voluptates saepe!</p>
-                        <button className='btn bg-blue-600 mt-5 px-7 py-4 text-white' >Read More</button>
+                        <button className=' bg-[#00D7FF] mt-5 px-7 py-4 text-black' >Read More</button>
                     </div>
                     <div>
                         <img src={student} alt="" className='w-[400px]' />
---
+++ src/Home/Navber.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/collage-logo.png'
+
+const Navber = () => {
+    const naveItem = <>
+        <div className=' lg:flex text-gray-900 font-light'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/'>Colleges</Link></li>
+            <li><Link to='/'>Admission</Link></li>
+            <li><Link to='/'>My College</Link></li>
+        </div>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {naveItem}
+                        </ul>
+                    </div>
+                    <Link to='/'>
+                        <img src={logo} alt="" className=' w-[150px]' />
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {naveItem}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navber;
